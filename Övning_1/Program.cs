2387dc36ac0a7e600160720772f483020b52f857
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-04-05 22:34:04 +0200

    Add project files.

---
+++ Övning_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Övning_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Brand = "Subaru";
+            myCar.Model = "Impreza";
+            myCar.Year = 2004;
+            myCar.Color = "Blå";
+            myCar.Price = 150000;
+
+            Console.WriteLine($"Min bil är en {myCar.Color} {myCar.Year} {myCar.Brand} {myCar.Model} som kostar {myCar.Price}kr");
+        }
+    }
+}
